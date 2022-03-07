7e060e7ea71fe0f96281d82a37e68018259cb3e6
simonchn160 <simonchn160@gmail.com>
2022-03-07 17:33:06 -0500

    add filtering

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
- const express = require('express');
+const express = require('express');
 const path = require('path');
 
 const PORT = 3000;
@@ -32,6 +32,17 @@ app.use((err, req, res) => {
   console.log(err);
   res.status(500).json(err);
 });
+
+// app.use((err, req, res, next) => {
+//   const defaultErr = {
+//     log: 'Express error handler caught unknown middleware error',
+//     status: 400,
+//     message: { err: 'An error occurred' },
+//   };
+//   const errorObj = Object.assign({}, defaultErr, err);
+//   console.log(errorObj.log);
+//   return res.status(errorObj.status).json(errorObj.message);
+// });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
